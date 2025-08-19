94156422f5df6c682e64c78d766a31865abbd84b
SANDEEP-KR-MAHTO <96254319+SANDEEP-KR-MAHTO@users.noreply.github.com>
2025-08-19 17:36:40 +0530

    Time: 2 ms (6.67%), Space: 11.6 MB (5.48%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        digits[n-1]++;
+
+        for(int i = n-1; i>0; i--){
+            if(digits[i] > 9){
+                digits[i] = 0;
+                digits[i-1]++;
+            }
+        }
+        if(digits[0]>9){
+            digits[0] = 0;
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+
+    }
+};
