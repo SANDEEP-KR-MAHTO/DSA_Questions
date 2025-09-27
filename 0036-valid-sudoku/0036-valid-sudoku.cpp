99455e85d313b33d36c122b54578a1363734ffad
SANDEEP-KR-MAHTO <96254319+SANDEEP-KR-MAHTO@users.noreply.github.com>
2025-09-27 16:14:36 +0530

    Time: 3 ms (51.35%), Space: 21.8 MB (70.64%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool rows[9][9] = {false};
+        bool cols[9][9] = {false};
+        bool boxes[9][9] = {false};
+
+        for(int i =0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] != '.'){
+                    int nums = board[i][j] - '1';
+                    int boxIndex = (i/3)*3 + (j/3);
+                    if (rows[i][nums] || cols[j][nums] || boxes[boxIndex][nums]){
+                        return false;
+                    }
+                    rows[i][nums] = cols[j][nums] = boxes[boxIndex][nums] = true;
+                }
+            }
+        }
+        return true;
+        
+    }
+};
