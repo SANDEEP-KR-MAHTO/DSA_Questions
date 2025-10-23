92ee143de73d01db739c53d9d7434ccb5789521c
SANDEEP-KR-MAHTO <96254319+SANDEEP-KR-MAHTO@users.noreply.github.com>
2025-10-23 19:39:30 +0530

    Time: 0 ms (100%), Space: 8.9 MB (3.91%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int i = 1;
-        int count = 0;
-        while(i<=n){
-            count += 1;
-            n=n-i;
-            i++;
-        }
-        return count;
-        
+        int low = 0;
+        int high = n;
+        while(low<=high){
+            long mid = low + (high-low)/2;
+            long coins = mid * (mid+1)/2;
+
+            if (coins == n){
+                return mid;
+            }
+            else if (coins < n){
+                low = mid + 1;
+            }
+            else{
+                high = mid-1;
+            }
+        }  
+        return high;      
     }
 };
