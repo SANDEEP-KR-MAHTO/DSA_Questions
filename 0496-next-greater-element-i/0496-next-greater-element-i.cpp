7a8c2ed6ffe62a05326cbaa50b57840af1f4f41e
SANDEEP-KR-MAHTO <96254319+SANDEEP-KR-MAHTO@users.noreply.github.com>
2025-11-05 08:37:55 +0530

    Time: 15 ms (5.08%), Space: 12.2 MB (96.35%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> l;
+        for(int i =0; i<nums1.size(); i++){
+            for(int j =0; j<nums2.size(); j++){
+                if(nums1[i] == nums2[j]){
+                    if(j==nums2.size()-1){
+                        l.push_back(-1);
+                    }
+                    else{
+                        bool flag=false;
+                       for(int k = j+1; k< nums2.size(); k++){
+                        if(nums2[j] < nums2[k]){
+                            l.push_back(nums2[k]);
+                            flag=true;
+                            break;
+                        }
+                       }
+                       if(!flag){
+                        l.push_back(-1);
+                       }
+                       
+                    }
+                }
+            }
+        }
+        return l;
+        
+    }
+};
