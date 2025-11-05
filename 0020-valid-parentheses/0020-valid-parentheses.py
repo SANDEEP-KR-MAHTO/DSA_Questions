40d4429ec75a40e2fcf9813a739ab94cb66c9b31
SANDEEP-KR-MAHTO <96254319+SANDEEP-KR-MAHTO@users.noreply.github.com>
2025-11-05 08:11:01 +0530

    Time: 0 ms (100%), Space: 17.8 MB (58.03%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -7,13 +7,11 @@ class Solution:
             else:
                 if len(st)==0:
                     return False
-                    break
                     
                 top = st[-1]
                 st.pop()
                 if (c == ')' and top != '(') or (c=='}' and top != '{') or (c==']' and top!= '['):
                     return False
-                    break
                     
 
         return len(st) == 0
