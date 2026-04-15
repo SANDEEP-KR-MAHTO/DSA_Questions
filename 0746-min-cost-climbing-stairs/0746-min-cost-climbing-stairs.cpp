d85264608daec95189d087ea2a0b88065ac7c05e
SANDEEP-KR-MAHTO <96254319+SANDEEP-KR-MAHTO@users.noreply.github.com>
2026-04-15 11:47:03 +0530

    Time: 4 ms (4.84%), Space: 17.6 MB (49.7%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        // dp[i] = minimum cost to reach step i
+        // i  = how to reach step i
+        // recursion relation
+        // base case
+        int n = cost.size();
+        vector<int> dp(n+1);
+        for(int i=2; i<=n; i++){
+            dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]);
+        }
+
+        return dp[n];
+    }
+};
