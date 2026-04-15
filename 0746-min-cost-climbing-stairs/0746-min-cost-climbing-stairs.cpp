160e2ad0dfd39ede87548450d1f6ae774ed5a519
SANDEEP-KR-MAHTO <96254319+SANDEEP-KR-MAHTO@users.noreply.github.com>
2026-04-15 11:48:30 +0530

    Time: 0 ms (100%), Space: 17.6 MB (71.94%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -7,6 +7,9 @@ public:
         // base case
         int n = cost.size();
         vector<int> dp(n+1);
+
+        dp[0]=0;
+        dp[1]=0;
         for(int i=2; i<=n; i++){
             dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]);
         }
